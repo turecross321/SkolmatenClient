e982227f08ce4d9d7163a92b07dadfa0bb3ff370
Ture <51852312+turecross321@users.noreply.github.com>
2024-09-11 16:11:43 +0200

    Add example

---
+++ SkolmatenApi.Examples/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using SkolmatenApi.Client;
+using SkolmatenApi.Types;
+
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+Logger<SkolmatenClient> logger = new Logger<SkolmatenClient>(factory);
+
+using SkolmatenClient client = new(logger, "CLIENTID", "CLIENTVERSIONID");
+var provinces = await client.GetProvincesAsync();
+List<School> allSchools = new List<School>();
+
+logger.LogInformation("Fetching every single school!");
+
+foreach (Province province in provinces)
+{
+    var districts = await client.GetDistrictsAsync(province);
+    foreach (District district in districts)
+    {
+        var schools = await client.GetSchoolsAsync(district);
+        allSchools.AddRange(schools);
+    }
+}
+
+foreach (School school in allSchools)
+{
+    Console.WriteLine($"[{school.District.Name}] {school.Name}");
+}
